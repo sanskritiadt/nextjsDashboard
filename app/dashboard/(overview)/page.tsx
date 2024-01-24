86a81a3cb8b203ff8efbb896f6491e504d5e14b9
sanskriti dhakad <sanskritidhakad.adt@gmail.com>
2024-01-24 17:17:54 +0530

    bug-resolved

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -10,10 +10,10 @@ import CardWrapper from '@/app/ui/dashboard/cards';
  
 export default async function Page() {
   //const revenue = await fetchRevenue();
-  const {totalPaidInvoices,
-    totalPendingInvoices,
-    numberOfInvoices,
-    numberOfCustomers} = await fetchCardData();
+  //const {totalPaidInvoices,
+    //totalPendingInvoices,
+   // numberOfInvoices,
+    //numberOfCustomers} = await fetchCardData();
 
 
   return (
@@ -36,10 +36,12 @@ export default async function Page() {
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
         {/* <RevenueChart revenue={revenue}  /> */}
+
         <Suspense fallback={<RevenueChartSkeleton />}>
           <RevenueChart />
         </Suspense>
         {/* <LatestInvoices latestInvoices={latestInvoices} /> */}
+        
         <Suspense fallback={<LatestInvoicesSkeleton />}>
           <LatestInvoices />
         </Suspense>
